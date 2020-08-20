7f33e3afcce9464b4cc8aa70a7896975b8f4fcfa
r48n34 <r48n34@gmail.com>
2020-08-21 03:52:27 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+var statData = {
+    awardNum : 8,
+    coopOrg : 3,
+    event : 20,
+    Man : 1
+    
+}
+
+function shows(){
+    alert(statData.awardNum);
+
+}
+function hello(){
+    
+}
+
+function yolo(){
+    document.getElementById("yolo").innerHTML = "HELLO";
+}
+
