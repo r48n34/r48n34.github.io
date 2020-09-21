f584de7c7e0ba477f42396bd23bc148d15258048
r48n34 <r48n34@gmail.com>
2020-09-22 06:43:16 +0800

    Update data.dev.js

--- randomVdo/dist/data.dev.js
+++ randomVdo/dist/data.dev.js
@@ -5,24 +5,30 @@
 var request = new XMLHttpRequest();
 request.open('GET', 'data.txt', false);
 request.send();
-var textData = request.responseText;
-var videoList = textData.split("\n"); //Array of the video list
+var textData = request.responseText; //original video list
 
-var playedVideo = [];
-var count = 0; //random to to a video
+var videoList = textData.split("\n"); //Array list of the video list
+
+var playedVideo = []; //Played video list
+
+var count = 0; //Played counter
+//random to to a video
 
 function goRandom() {
-  var num = Math.floor(Math.random() * videoList.length); //console.log(num);
+  var num = Math.floor(Math.random() * videoList.length); //rand num of the array
+  //console.log(num);
 
-  window.open(videoList[num], '_blank');
+  window.open(videoList[num], '_blank'); //Play video in a new windows
+
   playedVideo.push(videoList[num]);
   count++;
   console.log(playedVideo);
   document.getElementById("pvv").innerHTML = "Played Video : (" + count + ")";
   document.getElementById("lin").innerHTML += count + ": " + videoList[num] + "<br />";
-} //Output total video data
+}
 
+var updates = "Video DB last update(22/09/20) <br> Total Video currently = ";
 
 function total() {
-  document.getElementById("tt").innerHTML = "Video DB last update(22/09/20) <br> Total Video currently = " + videoList.length;
-} //console.log(videoList);
+  document.getElementById("tt").innerHTML = updates + videoList.length;
+}
