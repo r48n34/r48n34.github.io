3ffc1a4766929f2f9c7203e096276e9ac10162a0
r48n34 <r48n34@gmail.com>
2020-09-22 06:43:14 +0800

    Update data.js

--- randomVdo/data.js
+++ randomVdo/data.js
@@ -4,19 +4,19 @@
     var request = new XMLHttpRequest();
    request.open('GET', 'data.txt', false);
    request.send();
-   
-   var textData = request.responseText;
+  
+   var textData = request.responseText; //original video list
+   var videoList = textData.split("\n"); //Array list of the video list
 
-   var videoList = textData.split("\n"); //Array of the video list
-   var playedVideo = [];
+   var playedVideo = [];//Played video list
 
-    var count = 0;
+    var count = 0;//Played counter
 
     //random to to a video
     function goRandom(){
-        let num = Math.floor(Math.random() * videoList.length);
+        let num = Math.floor(Math.random() * videoList.length); //rand num of the array
         //console.log(num);
-        window.open(videoList[num], '_blank');
+        window.open(videoList[num], '_blank'); //Play video in a new windows
         playedVideo.push(videoList[num]);
         count ++;
 
@@ -28,13 +28,15 @@
 
     }
 
-    //Output total video data
+    
+
+    var updates = "Video DB last update(22/09/20) <br> Total Video currently = ";
     function total(){
-        document.getElementById("tt").innerHTML = "Video DB last update(22/09/20) <br> Total Video currently = " + videoList.length;  
+        document.getElementById("tt").innerHTML = updates + videoList.length;  
     }
     
 
-   //console.log(videoList);
+   
 
 
 
