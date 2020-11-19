204d1889c7ddf86dc7df5b4f491fe3e4e7bcba1d
r48n34 <r48n34@gmail.com>
2020-11-20 01:19:15 +0800

    Create playRand.dev.js

---
+++ randProj/dist/playRand.dev.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// Video of the random list
+//new method
+var request = new XMLHttpRequest();
+request.open('GET', 'songWebText.txt', false);
+request.send();
+var videoList = request.responseText.split("\n"); //Array list of the video list
+
+var playedVideo = []; //Played video list
+
+var count = 0; //Played counter
+//random to to a video
+
+function goRandom() {
+  var num = Math.floor(Math.random() * videoList.length); //rand num of the array
+
+  window.open(videoList[num], '_blank'); //Play video in a new windows
+
+  playedVideo.push(videoList[num]);
+  count++;
+  console.log(playedVideo);
+  document.getElementById("pvv").innerHTML = "Played Video : (" + count + ")";
+  document.getElementById("lin").innerHTML += count + ": " + videoList[num] + "<br />";
+}
+
+function total() {
+  document.getElementById("tt").innerHTML = "last update(20/11/20) <br> Total Video currently = " + videoList.length;
+}
