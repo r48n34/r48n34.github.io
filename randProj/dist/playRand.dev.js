3b326eb28da1cef95316e537b092e409ab1bde45
r48n34 <r48n34@gmail.com>
2020-11-22 21:55:48 +0800

    Update playRand.dev.js

--- randProj/dist/playRand.dev.js
+++ randProj/dist/playRand.dev.js
@@ -13,17 +13,27 @@ var count = 0; //Played counter
 //random to to a video
 
 function goRandom() {
-  var num = Math.floor(Math.random() * videoList.length); //rand num of the array
+  var num = Math.floor(Math.random() * videoList.length); //rand num of the array        
+  //even = video name || odd = vdo link
+  //if num = odd, no need to change num, else num need to +1 for get the link         
 
+  var vName = num & 1 ? videoList[num - 1] : videoList[num++];
   window.open(videoList[num], '_blank'); //Play video in a new windows
 
   playedVideo.push(videoList[num]);
-  count++;
-  console.log(playedVideo);
+  console.log(playedVideo[count++]);
   document.getElementById("pvv").innerHTML = "Played Video : (" + count + ")";
-  document.getElementById("lin").innerHTML += videoList[num] + "<br />";
+  document.getElementById("lin").innerHTML += vName.link(videoList[num]) + "<br/>";
 }
 
 function total() {
-  document.getElementById("tt").innerHTML = "last update(20/11/20) <br> Total Video currently = " + videoList.length;
+  //let total = (videoList.length & 1) ? (videoList.length / 2) - 0.5 : (videoList.length / 2); // odd even method
+  //let total = parseInt(videoList.length / 2, 10); // double to int method
+  document.getElementById("tt").innerHTML = "last update(22/11/20) <br> Total Video currently = " + parseInt(videoList.length / 2, 10);
 }
+
+document.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    goRandom();
+  }
+});
