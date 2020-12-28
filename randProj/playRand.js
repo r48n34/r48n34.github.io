958cf6a9c338db39b14a746487248308de4eb2c1
r48n34 <r48n34@gmail.com>
2020-12-28 17:24:54 +0800

    Update playRand.js

--- randProj/playRand.js
+++ randProj/playRand.js
@@ -1,46 +1,66 @@
 // Video of the random list
 
-    //new method
-    var request = new XMLHttpRequest();
-    request.open('GET', 'songWebText.txt', false);
-    request.send();
-   
-    var videoList = request.responseText.split("\n"); //Array list of the video list
-    var playedVideo = [];//Played video list
- 
-    var count = 0;//Played counter
- 
-     //random to to a video
-    function goRandom(){
-        
-         let num = Math.floor(Math.random() * videoList.length); //rand num of the array        
-         
-         //even = video name || odd = vdo link
-         //if num = odd, no need to change num, else num need to +1 for get the link         
-         var vName = (num & 1) ? videoList[num-1] : videoList[num++];
-                     
-         window.open(videoList[num], '_blank'); //Play video in a new windows
-         playedVideo.push(videoList[num]);
- 
-         console.log(playedVideo[count++]);       
- 
-         document.getElementById("pvv").innerHTML = "Played Video : (" + count + ")";
-         document.getElementById("lin").innerHTML += vName.link(videoList[num]) + "<br/>";
-          
-    }
- 
-    function total(){
-         //let total = (videoList.length & 1) ? (videoList.length / 2) - 0.5 : (videoList.length / 2); // odd even method
-         //let total = parseInt(videoList.length / 2, 10); // double to int method
-         document.getElementById("tt").innerHTML = "last update(29/11/20) <br> Total Video currently = " + parseInt(videoList.length / 2, 10);  
-    }
-
-    document.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        goRandom();
-    }
-    });
+class WebLink{ // class method to implementing
      
+     constructor(name, url){  
+         this.webName = name;
+         this.webUrl = url;
+     }
+ 
+ 
+}
+
+
+var request = new XMLHttpRequest();
+request.open('GET', 'songWebText.txt', false);
+request.send();
+   
+let videoList = request.responseText.split("\n"); //Array list of the video list
+let classList = [];
+
+for(let i = 0; i < videoList.length ; i += 2){
+     let temp = new WebLink (videoList[i], videoList[i+1]);
+     classList.push(temp);
+}
+
+videoList = [];
+
+var playedVideo = [];//Played video list
+var count = 0;//Played counter
+
+const toppic = document.getElementById("pvv");
+const below = document.getElementById("lin"); 
+ 
+
+function goRandom(){
+        
+     let num = Math.floor(Math.random() * classList.length); //rand num of the array        
+                
+     let vName = classList[num].webName;
+     let video = classList[num].webUrl;
+                    
+     window.open(video, '_blank'); //Play video in a new windows
+     playedVideo.push(video);
+ 
+     console.log(video);       
+ 
+     toppic.innerHTML = "Played Video : (" + playedVideo.length + ")";
+     below.innerHTML += vName.link(video) + "<br/>";
+          
+}
+ 
+function total(){
+     document.getElementById("tt").innerHTML = "last update(29/11/20) <br> Total Video currently = " + parseInt(classList.length, 10);  
+}
+
+document.addEventListener("keyup", function(event) {
+     if (event.keyCode === 13) {
+        goRandom();
+     }
+});
+
+
+
  
     
  
