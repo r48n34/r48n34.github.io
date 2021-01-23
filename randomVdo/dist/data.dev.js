b0a79d473b26a3535b69f52c20b6b209398d5eb9
r48n34 <r48n34@gmail.com>
2021-01-23 21:46:07 +0800

    Update data.dev.js

--- randomVdo/dist/data.dev.js
+++ randomVdo/dist/data.dev.js
@@ -1,7 +1,6 @@
 "use strict";
 
 // Video of the random list
-//new method
 var request = new XMLHttpRequest();
 request.open('GET', 'data.txt', false);
 request.send();
@@ -9,8 +8,7 @@ var videoList = request.responseText.split("\n"); //Array list of the video list
 
 var playedVideo = []; //Played video list
 
-var count = 0; //Played counter
-//random to to a video
+var time = request.getResponseHeader("Last-Modified").slice(4, 25); //random to to a video
 
 function goRandom() {
   var num = Math.floor(Math.random() * videoList.length); //rand num of the array
@@ -18,16 +16,15 @@ function goRandom() {
   window.open(videoList[num], '_blank'); //Play video in a new windows
 
   playedVideo.push(videoList[num]);
-  count++;
   console.log(playedVideo);
   var vName = videoList[num];
-  document.getElementById("pvv").innerHTML = "Played Video : (" + count + ")";
-  document.getElementById("lin").innerHTML += count + ": " + vName.link(videoList[num]) + "<br />";
+  document.getElementById("pvv").innerHTML = "Played Video : (" + playedVideo.length + ")";
+  document.getElementById("lin").innerHTML += playedVideo.length + ": " + vName.link(videoList[num]) + "<br />";
 }
 
-function total() {
-  document.getElementById("tt").innerHTML = "Video DB last update(23/11/20) <br> Total Video currently = " + videoList.length;
-}
+window.onload = function total() {
+  document.getElementById("tt").innerHTML = "Video DB last update: " + time + "<br> Total Video = " + videoList.length;
+};
 
 document.addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
