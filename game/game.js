7a012d04893b2c38ebfe93fc3542d37780493dfe
r48n34 <r48n34@gmail.com>
2021-04-01 01:11:52 +0800

    gm

---
+++ game/game.js
@@ -0,0 +1,222 @@
+const mainPage = document.getElementById("mainPage");
+const startBtnEasy = document.getElementById("startBtnEasy");
+const startBtnHard = document.getElementById("startBtnHard");
+
+const stagePage = document.getElementById("stagePage");
+const stageMessage = document.getElementById("stageMessage");
+
+const stageOne = document.getElementById("stageOne");
+const runppl = document.getElementById("runppl");
+const stageOneTimer = document.getElementById("stageOneTimer");
+
+const stageTwo = document.getElementById("stageTwo");
+const actionPic = document.getElementById("actionPic");
+const sTwoCount = document.getElementById("sTwoCount");
+const twoTopMessgae = document.getElementById("twoTopMessgae");
+
+const stageThree = document.getElementById("stageThree");
+const threeTimer = document.getElementById("threeTimer");
+const leftArr = document.getElementById("leftArr");
+const downArr = document.getElementById("downArr");
+const upArr = document.getElementById("upArr");
+const rightArr = document.getElementById("rightArr");
+
+
+let stagePosition = 0;
+
+let times = 0;
+let isRunning = false;
+
+//stage 1 running time
+let runningtime = 20; 
+
+//stage 2 variables
+let actionImg = ["kintore_ojisan.png","walk_boy_run.png", "banzai_kids_girl2.png", "fashion_outdoor_girl.png", "undou_sports_family_haha_musume.png","sick_alcohol_chudoku.png","tabako_denshi_ojisan.png"]; //stage 2 patten
+let doingtime = 30;
+let preNum = 0;
+let actionInverted = 6;
+
+//stage 3 variables
+let arrowImg = ["left.png", "down.png", "up.png", "right.png"];
+let musictime = 40;
+
+var interval = setInterval(function() { // timer function
+    if (isRunning) {
+
+        times ++;
+        console.log(times);
+
+        if(stagePosition == 1){
+            stageOneTimer.innerHTML = times + "/" +runningtime + " seconds";
+
+            if(times == runningtime + 1){
+                isRunning = false;
+                times = 0;
+                stageOneTimer.innerHTML = "Done! Great!"
+
+                setTimeout(goStage, 4000, 2 ,stageOne);
+                setTimeout(stageTwoGame, 8000);
+
+            }
+        }
+        else if(stagePosition == 2){
+
+            if(times % actionInverted == 0){
+                actionPic.innerHTML = "";
+
+                var img = document.createElement('img');
+                let num = Math.floor(Math.random() * actionImg.length);
+                let srcName = "img/" + actionImg[num];
+                console.log(srcName);
+
+                img.height = "400";
+
+
+                img.src = srcName;
+
+                actionPic.appendChild(img);
+                
+                if (preNum != num){
+                    twoTopMessgae.innerHTML = "Change!";
+                }
+                else{
+                    twoTopMessgae.innerHTML = "Hold!";
+                }
+
+                preNum = num;
+                
+
+            }
+
+            sTwoCount.innerHTML = (times % actionInverted) + 1;
+
+            if(times == doingtime){
+                isRunning = false;
+                times = 0;
+                sTwoCount.innerHTML = "Done! Great!"
+
+                setTimeout(goStage, 4000, 3 ,stageTwo);
+                setTimeout(stageThreeGame, 8000);
+
+
+            }
+
+        }
+        else if(stagePosition == 3){
+            threeTimer.innerHTML = times + " sec";
+
+            if(times % 3 == 0){
+
+                var img = document.createElement('img');
+                let num = Math.floor(Math.random() * arrowImg.length);
+                let srcName = "img/" + arrowImg[num];
+                console.log(srcName);               
+
+                img.src = srcName;
+                img.width = "200";
+                img.height = "200";            
+
+                let arrName = "a" + times;
+                img.id = arrName
+
+                img.className += "moveArrow";
+
+                if(num == 0){
+                    leftArr.appendChild(img);            
+                }
+                else if(num == 1){
+                    downArr.appendChild(img); 
+                }
+                else if(num == 2){
+                    upArr.appendChild(img); 
+                }
+                else{
+                    rightArr.appendChild(img);  
+                }
+                   
+                setTimeout(function(){ document.getElementById(arrName).style.display = "none"; }, 3000);
+
+
+                
+            }
+
+
+
+        }
+               
+    } 
+}, 1000);
+
+function goStage(number, curPage){
+    curPage.style.display = "none";
+
+    stageMessage.innerHTML = "Stage " + number + "<br>";
+    stagePage.style.display = "block";
+
+    setTimeout(function(){ stageMessage.innerHTML += "Ready" + "<br>"; }, 1500);
+    setTimeout(function(){ stageMessage.innerHTML += "Go!" + "<br>"; }, 2500); // 4000 time
+
+    setTimeout(function(){ stagePage.style.display = "none"; }, 4000);
+
+}
+
+startBtnEasy.onclick = function() {
+    goStage(1,mainPage);
+    setTimeout(stageOneGame, 4000);
+
+}
+
+startBtnHard.onclick = function() {
+    runningtime = 25;
+
+    doingtime = 45;
+    actionInverted = 4;
+
+    musictime = 60;
+
+    goStage(1,mainPage);
+    setTimeout(stageOneGame, 4000);
+
+}
+
+
+
+function stageOneGame(){ //stage 1 game start function
+    stagePosition = 1;
+    stageOne.style.display = "block";
+    runppl.className += "runningCss";
+    isRunning = true;
+
+}
+
+function stageTwoGame(){ //stage 2 game start function
+    stagePosition = 2;
+    stageTwo.style.display = "block";
+    isRunning = true;
+
+    actionPic.innerHTML = "";
+    sTwoCount.innerHTML = "1";
+    twoTopMessgae.innerHTML = "Change!"
+
+    var img = document.createElement('img');
+    let num = Math.floor(Math.random() * actionImg.length);
+    let srcName = "img/" + actionImg[num];
+    console.log(srcName);   
+
+    img.height = "400";
+
+    img.src = srcName;
+
+    actionPic.appendChild(img);
+    preNum = num; 
+
+}
+
+function stageThreeGame(){ //stage 1 game start function
+    stagePosition = 3;
+    stageThree.style.display = "block";
+    isRunning = true;
+
+    threeTimer.innerHTML = "0 sec";
+
+}
