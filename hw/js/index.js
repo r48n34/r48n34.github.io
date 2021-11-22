0a2dbb2575b3630d0441641db6a8cc740b9c5967
reemo <r48n34@gmail.com>
2021-11-22 22:36:36 +0800

    yo

--- hw/js/index.js
+++ hw/js/index.js
@@ -1,3 +1,3 @@
-const bottomText = document.getElementById("bottomText");
-const tod = getToday();
-bottomText.innerHTML = 'ITE3006: IT Essentials - Web (Today is: ' + tod + ')';
+const bottomText = document.getElementById("bottomText"); // get the bottom h4 text id
+const tod = getToday(); // get current date string from utili.js function getToday()
+bottomText.innerHTML = 'ITE3006: IT Essentials - Web (Today is: ' + tod + ')'; // insert the text to regarding #bottomText elements
--- hw/js/utili.js
+++ hw/js/utili.js
@@ -1,6 +1,7 @@
+// get current time, format to a readable text format, then return
 function getToday(){
-    const d = new Date();
-    const date = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate();
+    const d = new Date(); // get date
+    const date = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate(); // fix format
 
-    return date;
+    return date; //return
 }
