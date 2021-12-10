113c87cf2712e9f2e4ee8fe09ce208d7224613e6
reemo <r48n34@gmail.com>
2021-12-11 01:16:03 +0800

    date

---
+++ hw/js/formHandle.js
@@ -0,0 +1,24 @@
+const formId = document.getElementById("formId");
+const serverUrl = "http://localhost:8080"
+
+
+// Assume you can submit to server function
+formId.addEventListener('submit', (e)=> {
+    e.preventDefault();
+
+    const formObj = {
+        name: formId.ipName.value,
+        age: formId.ipAge.value,
+        gender: formId.ipGender.value,
+        content: formId.ipContent.value,
+        color: formId.ipColor.value,  
+    }
+
+    console.log(formObj)
+
+    // your submit logic
+
+    // goto success page
+    window.location.href = 'submitSuccess.html'
+
+})
