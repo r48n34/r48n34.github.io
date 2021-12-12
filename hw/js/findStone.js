e0946adddb4e6b056f0e2cd286b1b477fa319b34
reemo <r48n34@gmail.com>
2021-12-12 20:29:52 +0800

    update

--- hw/js/findStone.js
+++ hw/js/findStone.js
@@ -6,12 +6,13 @@ const input1 = document.getElementById("input1");
 
 // your data places
 let data = [
-  { name: "Apple", title: "so good"},
-  { name: "Orange", title: "so good"},
-  { name: "Pear" , title: "so good"},
-  { name: "Sushi", title: "so good"},
-  { name: "Cat", title: "so good"},
-  { name: "Dog", title: "so good"},
+  { name: "藍針水晶不可不知的知識", title: "你知唔知藍針係乜？", url: "https://www.instagram.com/p/CVKxwS_JCI3/", imgScr:"./image/blue1.png"},
+  { name: "新手指南:超七有新舊？ 乜係超八超十一？", title: "不同產地的月亮石差別有多大?", url: "https://www.instagram.com/p/COFkqabD5Wo/", imgScr:"./image/m2.png"},
+  { name: "新手指南: 閃靈鑽？", title: "草莓晶主體部份為白水晶/粉水晶，內含物如草莓籽而得名?", url: "https://www.instagram.com/p/CLEYkDgDPek/", imgScr:"./image/b3.png"},
+  { name: "新手指南: 金草莓？", title: "係我眼中都係愛不惜手嘅寶貝✨實在冇必要因為咁而傷和氣呢～", url: "https://www.instagram.com/p/CPxtIR1jme8/", imgScr:"./image/b4.jpg"},
+  { name: "金運石的五個驚人冷知識", title: "睇完哩五個冷知識有咩感覺？", url: "https://www.instagram.com/p/CSHFPpOpjT2/", imgScr:"./image/b5.jpg"},
+
+
 ];
 
 // active function when user type on the input box
@@ -31,11 +32,16 @@ function genBox(data) {
   for (let i of data) {
     str += `
         <div>
-            <div style="width: 300px;">       
+            <div style="width: 600px;">       
                 <div>
                     <h2>${i.name}</h2>
-                    <p>${i.name}</p>
-                    <a href="#" class="selectClass" >Go somewhere</a>
+                    <p>${i.title}</p>
+                    <a href=${i.url} class="selectClass" target="_blank">
+                    <img src=${i.imgScr} alt="" width="300px" height="300px">
+                    <br/>
+                    know more
+                    </a>
+
                 </div>
             </div>
         </div>`;
