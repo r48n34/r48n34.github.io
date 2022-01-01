bd306696d571798ed78544c95d063e93ff1f56bc
reemo <r48n34@gmail.com>
2022-01-02 00:48:41 +0800

    test

---
+++ labo/public/component---src-pages-small-comp-sm-box-js-caacc02a83868e21ea2e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_gatsby_site=self.webpackChunkmy_gatsby_site||[]).push([[241],{8704:function(e,t,n){n.r(t);var u=n(5785),c=n(7294),a=[1,2,4,5,6];t.default=function(){var e=(0,c.useState)(a),t=e[0],n=e[1];return c.createElement("div",null,c.createElement("button",{onClick:function(){return n([].concat((0,u.Z)(t),[t[t.length-1]+1]))}}," click me "),t.map((function(e,t){return c.createElement("h5",{key:t},"About Me ",e)})))}}}]);
+//# sourceMappingURL=component---src-pages-small-comp-sm-box-js-caacc02a83868e21ea2e.js.map
