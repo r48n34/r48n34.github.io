56f658a2a4d3e525f4cd1cff4633ea9c7bd109fb
reemo <r48n34@gmail.com>
2022-09-07 02:55:48 +0800

    add animations

--- src/components/react/homePages/MainComp.tsx
+++ src/components/react/homePages/MainComp.tsx
@@ -1,14 +1,29 @@
 import { motion } from "framer-motion"
 import { Text } from '@mantine/core';
+import { Slide } from "react-awesome-reveal";
 
 function MainComp(){
     return (
+        <Slide duration={2000} >
         <motion.div 
             style={{ margin:0, lineHeight: 0.8, cursor: "pointer" }}
             onClick={ () => {
                 !!window && window.open("https://www.r48n34.me/", '_blank')
             }}
         >
+            <motion.h3
+                style={{ margin:0, textAlign: "right", marginBottom: "-26px" }}
+                animate={{ letterSpacing: ["2px", "4px"] }}
+                transition={{ ease: "easeInOut", duration: 3, repeat: Infinity, repeatType:"reverse" }}
+            >
+                <Text
+                    variant="gradient"
+                    gradient={{ from: 'indigo', to: 'cyan', deg: 45 }}
+                >
+                    r48n34.me
+                </Text>
+            </motion.h3>
+
             <motion.h1
                 style={{ margin:0 }}
                 animate={{ wordSpacing: ["2px", "15px"] }}
@@ -25,7 +40,7 @@ function MainComp(){
             <motion.h2
                 style={{ marginTop: "-26px" }}
                 // animate={{ wordSpacing: ["2px", "15px"] }}
-                animate={{ letterSpacing: ["2px", "10px"] }}
+                animate={{ letterSpacing: ["2px", "8px"] }}
                 transition={{ ease: "easeInOut", duration: 4, repeat: Infinity, repeatType:"reverse" }}
             >
                 <Text
@@ -36,6 +51,7 @@ function MainComp(){
                 </Text>
             </motion.h2>
         </motion.div>
+        </Slide>
     )
 }
     
